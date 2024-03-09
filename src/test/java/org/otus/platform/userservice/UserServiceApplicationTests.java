0653b1be1571f123c0839bfb3dad695a0eac14c4
Karen Farmanyan <farmanian.k@yandex.ru>
2024-03-09 09:43:19 +0300

    first commit

---
+++ src/test/java/org/otus/platform/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.otus.platform.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
